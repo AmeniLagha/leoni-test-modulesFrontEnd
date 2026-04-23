e130659499d7649fa24697bdaf212a5ed51a7523
AmeniLagha <laghaamani3@gmail.com>
2026-04-23 19:14:31 +0100

    pour serveur

--- src/environments/environment.docker.ts
+++ src/environments/environment.docker.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl:  'https://app-90097f72-09dd-4c01-a15f-6656da11d0a2.cleverapps.io'  // Utilise le proxy Nginx en production
+  apiUrl:  ''  // Utilise le proxy Nginx en production
 };
